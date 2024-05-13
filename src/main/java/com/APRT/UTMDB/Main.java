be2ca2c9e131c96a78999ff549dec8c4ade45ab6
JohnRichard4096 <windowserror@163.com>
2024-05-13 23:22:02 +0800

    1

--- src/main/java/com/APRT/UTMDB/Main.java
+++ src/main/java/com/APRT/UTMDB/Main.java
@@ -59,12 +59,58 @@ public class Main {
         while(!Objects.equals(Cmd, "exit")){
 
             System.out.print(">");
-            Cmd = in.next();
+            Cmd = in.nextLine();
             System.out.println();
             if(Objects.equals(Cmd, "exit")){
                 System.out.println("Bye");
                 break;
             }
+            if(Objects.equals(Cmd, "Ver") || Cmd.equals("Version")){
+                System.out.println("""
+                        UTM-DB 1.0 SnapShot
+                        By QiLingYuJie-John
+                        Welcome!
+                        """);
+            }
+            if(Objects.equals(Cmd, "reload")) {
+                try {
+                    // 读取资源文件
+                    InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("config.yml");
+                    if (inputStream == null) {
+                        System.out.println("Can not find config!");
+                        LLogger.LogRec("Can not find config!");
+                        return;
+                    }
+
+                    // 创建目标文件夹
+                    File destinationFolder = new File(destinationFolderPath);
+                    if (!destinationFolder.exists()) {
+                        destinationFolder.mkdirs();
+                    }
+
+                    // 写入文件到目标文件夹
+                    OutputStream outputStream = new FileOutputStream(destinationFolderPath + "config.yml");
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) > 0) {
+                        outputStream.write(buffer, 0, length);
+                    }
+
+                    // 关闭流
+                    inputStream.close();
+                    outputStream.close();
+
+                    System.out.println("Created config");
+                    LLogger.LogRec("Created config");
+                } catch (IOException e) {
+                    logger.warning("Error while creating config!!!");
+                    LLogger.LogRec("Error while creating config!!!");
+                    e.printStackTrace();
+
+                }
+                System.out.print("Complete!");
+            }
+
 
         }
 
